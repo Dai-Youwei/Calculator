51db5ecb1042221345b8cad9717b3861e0584628
Chase <dai12@tju.edu.cn>
2019-06-09 16:33:40 +0800

    first commit

---
+++ Calculator/Help.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Calculator
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_back = new System.Windows.Forms.Button();
+            this.content = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_back
+            // 
+            this.button_back.BackColor = System.Drawing.Color.Orange;
+            this.button_back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_back.Location = new System.Drawing.Point(317, 213);
+            this.button_back.Margin = new System.Windows.Forms.Padding(4);
+            this.button_back.Name = "button_back";
+            this.button_back.Size = new System.Drawing.Size(94, 38);
+            this.button_back.TabIndex = 0;
+            this.button_back.Text = "back";
+            this.button_back.UseVisualStyleBackColor = false;
+            this.button_back.Click += new System.EventHandler(this.button_back_Click);
+            // 
+            // content
+            // 
+            this.content.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.content.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.content.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.content.Location = new System.Drawing.Point(1, 12);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.ReadOnly = true;
+            this.content.Size = new System.Drawing.Size(827, 220);
+            this.content.TabIndex = 1;
+            this.content.Text = "   MS: memory save   存入存储\r\n   MR: memory read  读取存储\r\n   MC: memory clear  清除存储\r\n " +
+    "  M+: 在存储内加上\r\n   M-: 在存储内减去\r\n   AC: 重置";
+            this.content.TextChanged += new System.EventHandler(this.content_TextChanged);
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(840, 261);
+            this.Controls.Add(this.button_back);
+            this.Controls.Add(this.content);
+            this.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Help";
+            this.Opacity = 0.9D;
+            this.Text = "Help";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_back;
+        private System.Windows.Forms.TextBox content;
+    }
+}
